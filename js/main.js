d53732e5695a3e0ff60377ecd7534e32716f8889
nayannelbatista <nayannelopes.bd@gmail.com>
2024-06-20 11:03:03 -0300

    Mão na massa - botão cancelar

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
   ui.renderizarPensamentos()
 
   const formularioPensamento = document.getElementById("pensamento-form")
+  const botaoCancelar = document.getElementById("botao-cancelar")
+
   formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+  botaoCancelar.addEventListener("click", manipularCancelamento)
 })
 
 async function manipularSubmissaoFormulario(event) {
@@ -21,4 +24,8 @@ async function manipularSubmissaoFormulario(event) {
   catch {
     alert("Erro ao salvar pensamento")
   }
+}
+
+function manipularCancelamento() {
+  ui.limparFormulario();
 }
